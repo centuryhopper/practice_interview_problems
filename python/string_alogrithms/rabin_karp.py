f30530df79207aa1847b6fc06d2e91bb7400e5e5
Leo Zhang <leotheasianlion@gmail.com>
2022-11-14 17:12:26 -0500

    update

--- python/string_alogrithms/rabin_karp.py
+++ python/string_alogrithms/rabin_karp.py
@@ -5,23 +5,24 @@ t = abcdabcda
 p = cda
 '''
 
+
+# we can mod our hash function to avoid overflows in certain programming languages
 PRIME = 101
 
 # assume lowercase english letters
 def rabinKarp(text, pat):
+    n = len(text)
+    m = len(pat)
     def makeHash(s: str) -> int:
         if not s.isalpha():
             return -1
-        return sum((ord(c)-97) * (PRIME**i) for i,c in enumerate(s))
-    def recomputeHash(s: str, oldIndex: int, newIndex: int, oldHash: int, patternLen: str):
-        newHash = oldHash - (ord(s[oldIndex]) - 97)
-        newHash /= PRIME
-        # print(f'{newIndex = }')
-        newHash += (ord(s[newIndex]) - 97) * (PRIME ** (patternLen-1))
+        return sum((ord(c)-97) * (PRIME**(m-1-i)) for i,c in enumerate(s))
+    def recomputeHash(s: str, oldIndex: int, newIndex: int, oldHash: int):
+        newHash = oldHash - ((ord(s[oldIndex]) - 97) * PRIME**(m-1))
+        newHash *= PRIME
+        newHash += (ord(s[newIndex]) - 97)
         return newHash
 
-    n = len(text)
-    m = len(pat)
     patHash = makeHash(pat)
     textHash = makeHash(text[:m])
     # print(patHash, textHash)
@@ -36,7 +37,7 @@ def rabinKarp(text, pat):
                 # print(f'matched {pat}!')
                 ans.append(i)
         # min call to avoid out of bounds
-        textHash = recomputeHash(text, i, min(i+m, n-1), textHash, m)
+        textHash = recomputeHash(text, i, min(i+m, n-1), textHash)
 
     return ans
 
