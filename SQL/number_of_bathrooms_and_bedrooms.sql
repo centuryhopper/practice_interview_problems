7105f1b2d7bae063bbb91cbf6cbcd82291e54ba0
Leo Zhang <leotheasianlion@gmail.com>
2022-11-22 08:44:16 -0500

    Create number_of_bathrooms_and_bedrooms.sql

---
+++ SQL/number_of_bathrooms_and_bedrooms.sql
@@ -0,0 +1,4 @@
+select city, property_type, avg(bathrooms) as n_bathrooms_avg, avg(bedrooms) as n_bedrooms_avg from airbnb_search_details
+group by city, property_type
+
+-- https://platform.stratascratch.com/coding/9622-number-of-bathrooms-and-bedrooms?code_type=1
