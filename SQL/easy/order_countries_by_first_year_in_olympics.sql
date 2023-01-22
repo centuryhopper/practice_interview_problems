217b2f28c18caf71dce87e6b460b41de38da0595
Leo Zhang <leotheasianlion@gmail.com>
2023-01-21 23:30:53 -0500

    Create order_countries_by_first_year_in_olympics.sql

---
+++ SQL/easy/order_countries_by_first_year_in_olympics.sql
@@ -0,0 +1,4 @@
+select noc, min(year) first_time_year from olympics_athletes_events
+group by noc
+
+-- https://platform.stratascratch.com/coding/10184-order-all-countries-by-the-year-they-first-participated-in-the-olympics?code_type=1
